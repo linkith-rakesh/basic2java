3efec796990e56c07563109f6c052b2df6e87e4e
linkith-rakesh <103627927+linkith-rakesh@users.noreply.github.com>
2022-07-30 09:12:49 -0500

    Add files via upload

---
+++ BooleansComparison.java
@@ -0,0 +1,16 @@
+public class BooleansComparison {
+    public static void main(String[] args) {
+        int chemistry=88;
+        int biology = 90;
+        int maths=100;
+        int physics=90;
+        int computerScience=100;
+        System.out.println("Maths mark is greater than biology "+(maths>biology));
+        System.out.println("Chemistry mark is greater than biology "+(biology<chemistry));
+        System.out.println("Physics and computer science marks are eyual "+(physics==computerScience));
+        System.out.println("Maths and computerscience marks are equal "+(maths==computerScience));
+
+        
+    }
+    
+}
---
+++ IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int mathsGrade =100;
+        int computerScienceGrade = 99;
+        if(computerScienceGrade>=mathsGrade){
+System.out.println("computer mark is less than maths");             
+        }
+        else{
+            System.out.println("Maths mark is greater than computer");
+        }
+    }
+    
+}
